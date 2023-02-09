9b0b4e8cc2dca07f0d96d824ad7d33745482aab2
julieta77 <104325296+julieta77@users.noreply.github.com>
2023-02-09 11:01:13 -0300

    Listo para predecir

--- predict.py
+++ predict.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 
 
-longitud, altura = 100,100
+
+longitud, altura = 150,150
 modelo = './modelo/modelo.h5'
 pesos_modelo = './modelo/pesos.h5'
 cnn = tf.keras.models.load_model(modelo)
@@ -16,7 +17,13 @@ def predict(file):
     array = cnn.predict(x)
     result = array[0]
     answer = np.argmax(result)
+    dicc = {'american_football': 0, 'baseball': 1, 'basketball': 2, 'billiard_ball': 3, 'bowling_ball': 4, 'cricket_ball': 5, 'football': 6, 'golf_ball': 7, 'hockey_ball': 8, 'hockey_puck': 9, 'rugby_ball': 10, 'shuttlecock': 11, 'table_tennis_ball': 12, 'tennis_ball': 13, 'volleyball': 14}
+    for key, value in dicc.items():
+        if value == answer:
+            print(key)
     return answer
     
 
-print(predict('a.jpeg'))
+
+#print(predict('teni2.jpeg'))
+
